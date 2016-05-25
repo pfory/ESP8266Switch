28bae2b3f74e9f1de8d0a1ef417637171bed982e
pfory <pfory@seznam.cz>
2016-05-25 23:10:39 +0200

    +

--- switch2.lua
+++ switch2.lua
@@ -1,5 +1,5 @@
 --Init  
-base = "/home/Switch2/"
+base = "/home/Switch2/esp02/"
 deviceID = "ESP8266 Switch "..node.chipid()
 
 wifi.setmode(wifi.STATION)
@@ -8,7 +8,13 @@ wifi.sta.autoconnect(1)
 
 Broker="88.146.202.186"  
 
-heartBeat = node.bootreason()
+versionSW             = "0.1"
+versionSWString       = "Switch v" 
+print(versionSWString .. versionSW)
+
+heartBeat = node.bootreason()+10
+uart.write(0,"Boot reason:")
+print(heartBeat)
 
 pinRelay = 4 --GPIO2
 gpio.mode(pinRelay,gpio.OUTPUT)  
@@ -19,17 +25,28 @@ function reconnect()
   if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
     print ("Wifi Up!")
     tmr.stop(1) 
-    m:connect(Broker, 31883, 0, function(conn) 
+    m:connect(Broker, 31883, 0, 1, function(conn) 
       print(wifi.sta.getip())
       print("Mqtt Connected to:" .. Broker) 
       mqtt_sub() --run the subscription function 
     end)
   end
+  heartBeat=20
+  sendHB()
+end
+
+function sendHB()
+  print("I am sending HB to OpenHab")
+  m:publish(base.."HeartBeat",   heartBeat,1,0)
+
+  if heartBeat==0 then heartBeat=1
+  else heartBeat=0
+  end
 end
 
 
 function mqtt_sub()  
-  m:subscribe(base.."p1/com",0, function(conn)   
+  m:subscribe(base.."com",0, function(conn)   
     print("Mqtt Subscribed to OpenHAB feed for device "..deviceID)  
   end)  
 end  
@@ -43,20 +60,10 @@ m:on("offline", function(con)
   end)
 end)  
 
-
-function sendData()
-  print("I am sending data to OpenHab")
-  m:publish(base.."HeartBeat",   heartBeat,0,0)
-  if heartBeat==0 then heartBeat=1
-  else heartBeat=0
-  end
-end
-
-
  -- on publish message receive event  
 m:on("message", function(conn, topic, data)   
   print("Received:" .. topic .. ":" .. data) 
-  if topic == base.."p1/com" then
+  if topic == base.."com" then
     if data == "ON" then
       print("Relay ON")
       gpio.write(pinRelay,gpio.HIGH)  
@@ -67,17 +74,20 @@ m:on("message", function(conn, topic, data)
   end
 end)  
 
+uart.write(0,"Connecting to Wifi")
 tmr.alarm(0, 1000, 1, function() 
-  print ("Connecting to Wifi... ")
+  uart.write(0,".")
   if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
     print ("Wifi connected")
     tmr.stop(0) 
-    m:connect(Broker, 31883, 0, function(conn) 
+    m:connect(Broker, 31883, 0, 1, function(conn) 
       mqtt_sub() --run the subscription function 
       print(wifi.sta.getip())
       print("Mqtt Connected to:" .. Broker.." - "..base) 
+      m:publish(base.."VersionSW",              versionSW,0,0)  
+      sendHB()
       tmr.alarm(2, 60000, 1, function()  
-        sendData()
+        sendHB()
       end)
     end) 
   end
