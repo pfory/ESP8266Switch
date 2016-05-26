fb5ccf3b2eed8f6bbd2dc90ee8dbc068c8ab867c
pfory <pfory@seznam.cz>
2016-05-26 14:44:47 +0200

    +safety function

--- switch.lua
+++ switch.lua
@@ -2,13 +2,19 @@
 base = "/home/Switch1/esp01/"
 deviceID = "ESP8266 Switch "..node.chipid()
 
+heartBeat = node.bootreason() + 10
+print("Boot reason:"..heartBeat)
+
 wifi.setmode(wifi.STATION)
 wifi.sta.config("Datlovo","Nu6kMABmseYwbCoJ7LyG")
+cfg={
+  ip = "192.168.1.154",
+  netmask = "255.255.255.0",
+  gateway = "192.168.1.1"
+}
 wifi.sta.autoconnect(1)
 
 Broker="88.146.202.186"  
-
-heartBeat = node.bootreason() + 10
 
 versionSW         = 0.4
 versionSWString   = "Switch v" 
@@ -23,7 +29,7 @@ function reconnect()
   if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
     print ("Wifi Up!")
     tmr.stop(1) 
-    m:connect(Broker, 31883, 0, function(conn) 
+    m:connect(Broker, 31883, 0, 1, function(conn) 
       print(wifi.sta.getip())
       print("Mqtt Connected to:" .. Broker) 
       mqtt_sub() --run the subscription function 
@@ -57,7 +63,6 @@ function sendData()
   end
 end
 
-
 -- on publish message receive event  
 m:on("message", function(conn, topic, data)   
   print("Received:" .. topic .. ":" .. data) 
@@ -65,6 +70,9 @@ m:on("message", function(conn, topic, data)
     if data == "ON" then
       print("Relay ON")
       gpio.write(pinRelay,gpio.HIGH)  
+      tmr.alarm(5, 3600000, 1, function()  --safety function
+        gpio.write(pinRelay,gpio.LOW)  
+      end)
     else
       print("Relay OFF")
       gpio.write(pinRelay,gpio.LOW)  
@@ -72,12 +80,13 @@ m:on("message", function(conn, topic, data)
   end
 end)  
 
+uart.write(0,"Connecting to Wifi")
 tmr.alarm(0, 1000, 1, function() 
-  print ("Connecting to Wifi... ")
+  uart.write(0,".")
   if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
     print ("Wifi connected")
     tmr.stop(0) 
-    m:connect(Broker, 31883, 0, function(conn) 
+    m:connect(Broker, 31883, 0, 1, function(conn) 
       mqtt_sub() --run the subscription function 
       print(wifi.sta.getip())
       print("Mqtt Connected to:" .. Broker.." - "..base) 
